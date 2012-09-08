b8cdca10b61fe365b5949ab7d5c599c811faf05a
Gagan Preet Singh <s.gagan.preet@gmail.com>
2012-09-08 17:43:29 +0530

    routing for admin

---
+++ application/controllers/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+class Home_Controller extends Base_Controller {
+
+	public function action_settings()
+	{
+		return View::make('admin.settings');
+	}
+
+	public function action_result()
+	{
+		return View::make('admin.results')
+	}
+
+}
